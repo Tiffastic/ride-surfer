90c69fc71535de7f795f6d2f4c334d217eb5c030
rachelbrough <rachel.brough@gmail.com>
2018-11-18 16:38:58 -0700

    fixed table name

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/cool', (req, res) => res.send(cool()))
 app.get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM users');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
