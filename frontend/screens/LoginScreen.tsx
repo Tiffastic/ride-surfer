bd076242c5774344625de5d2abcf04cb08423ae5
Thuy Nguyen <tiffastic@hotmail.com>
2018-11-27 17:02:30 -0700

    Erased Robot image selection and only loads up surfboard image now.

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -1,170 +1,170 @@
 
 import React from 'react';
 import {
-    Image,
-    ScrollView,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = {
-        header: null,
-    };
+  static navigationOptions = {
+    header: null,
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-                    <View style={styles.welcomeContainer}>
-                        <Image
-                            source={
-                                __DEV__
-                                    ? require('../assets/images/Loginsurfboard.png')//require('../assets/images/robot-dev.png')
-                                    : require('../assets/images/robot-prod.png')
-                            }
-                            style={styles.welcomeImage}
-                            />
-                    </View>
+  render() 
+  {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                require('../assets/images/Loginsurfboard.png')             
+              } 
+              style={styles.welcomeImage}
+            />
+          </View>
 
-                    <View style={styles.getStartedContainer}>
+          <View style={styles.getStartedContainer}>           
 
-                        <Text style={styles.getStartedText}>Log into Ride Surfer </Text>
+            <Text style={styles.getStartedText}>Log into Ride Surfer </Text>
 
-                        <View style={styles.loginContainer}>
-                            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-                                <Text>Email: </Text>
-                                <TextInput placeholder='rideSurfer@waves.com' style={styles.input}></TextInput>
-                            </View>
+              <View style={styles.loginContainer}>              
+                  <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+                    <Text>Email:</Text>
+                    <TextInput placeholder='rideSurfer@waves.com' style={styles.input}></TextInput>
+                  </View>
 
-                            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-                                <Text>Password: </Text>
-                                <TextInput placeholder='********' style={styles.input}></TextInput>
-                            </View>
+                  <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+                    <Text>Password: </Text>
+                    <TextInput placeholder='********' style={styles.input}></TextInput>
+                  </View>
 
-                        </View>
-
-
-                    </View>
-
-                    <View style={styles.surfButton}>
-                        <Button  title='Log in!' onPress={this._handleLearnMorePress}></Button>
-                    </View>
-                </ScrollView>
-
-            </View>
-        );
-    }
+              </View>
 
 
+          </View>
 
-    _handleLearnMorePress = () => {
-        WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-    };
+          <View style={styles.surfButton}>        
+              <Button  title='Log in!' onPress={this._handleLearnMorePress}></Button>
+          </View>
+        </ScrollView>
+
+      </View>
+    );
+  }
 
 
+
+  _handleLearnMorePress = () => 
+  {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  
 }
 
 const styles = StyleSheet.create({
-    container:
-    {
-        flex: 1,
-        backgroundColor: '#34889b'
-    },
+  container: 
+  {
+      flex: 1,
+      backgroundColor: '#34889b'
+  },
 
-    contentContainer:
-    {
-        paddingTop: 30,
-    },
+  contentContainer: 
+  {
+    paddingTop: 30,
+  },
+  
+  welcomeContainer: 
+  {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
 
-    welcomeContainer:
-    {
-        alignItems: 'center',
-        marginTop: 10,
-        marginBottom: 10,
-    },
+  welcomeImage: 
+  {
+    width: 200,
+    height: 150,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
 
-    welcomeImage:
-    {
-        width: 200,
-        height: 150,
-        resizeMode: 'contain',
-        marginTop: 3,
-        marginLeft: -10,
-    },
+  getStartedContainer: 
+  {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
 
-    getStartedContainer:
-    {
-        alignItems: 'center',
-        marginHorizontal: 50,
-    },
+  getStartedText: 
+  {
+    fontSize: 17,
+    color: 'black',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
 
-    getStartedText:
-    {
-        fontSize: 17,
-        color: 'black',
-        lineHeight: 24,
-        textAlign: 'center',
-    },
+  homeScreenFilename: 
+  {
+    marginVertical: 7,
+  },
 
-    homeScreenFilename:
-    {
-        marginVertical: 7,
-    },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingTop: 5
+  },
 
-    codeHighlightContainer: {
-        backgroundColor: 'rgba(0,0,0,0.05)',
-        borderRadius: 10,
-        paddingHorizontal: 10,
-        paddingTop: 5
-    },
+  logoContainer: {
+      alignItems: 'center',
+      flexGrow: 1,
+      justifyContent: 'center'
+  },
 
-    logoContainer: {
-        alignItems: 'center',
-        flexGrow: 1,
-        justifyContent: 'center'
-    },
+  logo: 
+  {
+      width: 100,
+      height: 100
+  },
 
-    logo:
-    {
-        width: 100,
-        height: 100
-    },
+  title: {
+      color: '#FFF',
+      marginTop: 10,
+      width: 160,
+      textAlign: 'center',
+      opacity: 0.9
+  },
 
-    title: {
-        color: '#FFF',
-        marginTop: 10,
-        width: 160,
-        textAlign: 'center',
-        opacity: 0.9
-    },
+  input:
+  {
+      height: 40,
+      backgroundColor:  'rgba(255, 255, 255, 0.7)',
+      marginBottom: 20,
+      color: '#2677A2',
+      paddingHorizontal: 70,
+  },
 
-    input:
-    {
-        height: 40,
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
-        marginBottom: 20,
-        color: '#2677A2',
-        paddingHorizontal: 70,
-    },
+  loginContainer:
+  {
+    paddingTop: 15,
+    paddingBottom: 10
+  },
 
-    loginContainer:
-    {
-        paddingTop: 15,
-        paddingBottom: 10
-    },
+  surfButton:
+  {
 
-    surfButton:
-    {
+    alignItems: 'center',
 
-        alignItems: 'center',
-
-    }
+  }
 
 
 });
