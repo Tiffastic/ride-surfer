1dcd097af0486396b12c6804d1ff299179271db5
Osama Kergaye <okergaye@gmail.com>
2018-12-07 11:21:51 -0700

    Header icons

--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -10,9 +10,11 @@ import {
   Button,
   View,
 } from 'react-native';
-import { createStackNavigator} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 
 import Colors from '../constants/Colors';
+import HeaderButtons, { HeaderButton } from 'react-navigation-header-buttons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import DriverPickerScreen from './DriverPickerScreen';
 import DriverDetailsScreen from './DriverDetailsScreen';
@@ -43,17 +45,16 @@ const dummyAutofill = [
   },
 ];
 
-class AddressPicker extends React.Component<{navigation: any}> {
-  static navigationOptions = {
-    title: 'Ride Surfer', // can be overidden from inside the stack
-
-  };
+class AddressPicker extends React.Component<{ navigation: any }> {
+  // static navigationOptions = {
+  //   title: 'Ride Surfer', // can be overidden from inside the stack
+  // };
   state = {
     text: '',
   };
 
   _onPress(item: any) {
-    this.setState({text: ''});
+    this.setState({ text: '' });
     this.props.navigation.push('DriverPicker', { address: item });
   }
 
@@ -92,33 +93,52 @@ class AddressPicker extends React.Component<{navigation: any}> {
   }
 } // end of address picker class
 
+
+const IoniconsHeaderButton = (passMeFurther: any) => (
+  // the `passMeFurther` variable here contains props from <Item .../> as well as <HeaderButtons ... />
+  // and it is important to pass those props to `HeaderButton`
+  // then you may add some information like icon size or color (if you use icons)
+  <HeaderButton {...passMeFurther} IconComponent={Ionicons} iconSize={40} color={Colors.primary} buttonStyle={{
+    // backgroundColor: "rgba(92, 99,216, 1)",
+    height: 60,
+    // textAlignVertical: 'center',
+
+    // borderWidth: 0,
+    // borderRadius: 5
+  }} />
+);
+
 export default createStackNavigator(
   {//RouteConfigs
 
     AddressPicker: {
       screen: AddressPicker,
       navigationOptions: ({ navigation }: { navigation: any }) => ({
-        //  title: `from inside the stack`,
+        title: `Ride Surfer`,
         headerRight: (
-          <View style={{width: 90}}>
-            <Button
-              onPress={() => navigation.push('MessageContacts')}
-              title="Messages"
-              color={Colors.primary}
-            />
-          </View>
+          <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+            <HeaderButton title="MessagesIcon" iconName="ios-chatbubbles" onPress={() => navigation.push('MessageContacts')} />
+          </HeaderButtons>
         ),
+
+        // headerLeft: (
+        //   <View style={{ width: 90 }}>
+        //     <Button
+        //       onPress={() => navigation.push('ProfileScreen')}
+        //       title="Profile"
+        //       color={Colors.primary}
+        //     />
+        //   </View>
+        // ),
         headerLeft: (
-          <View style={{width: 90}}>
-            <Button
-              onPress={() => navigation.push('ProfileScreen')}
-              title="Profile"
-              color={Colors.primary}
-            />
-          </View>
+          <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+            <HeaderButton title="ProfileIcon" iconName="ios-person" onPress={() => navigation.push('ProfileScreen')} />
+          </HeaderButtons>
         ),
         headerTitleStyle: {
           textAlign: 'center',
+          fontWeight: 'bold',
+          height: 45,
           flex: 1,
         }
       }),
@@ -131,17 +151,22 @@ export default createStackNavigator(
     RideInProgress: RideInProgressScreen,
     RateDriver: RateDriverScreen
 
-  }, {//StackNavigatorConfig
+  }, {//StackNavigatorConfig (Changes the bar itself and not the items inside it)
 
     initialRouteName: 'AddressPicker',
     // headerMode: 'none',
     navigationOptions: {//in react nav ver 3, this is called defaultNavigationOptions
       headerStyle: {
         backgroundColor: 'white',
+        height: 45,
+        // margin: 10, //this makes it clear what exactly StackNavigatorConfig is modifing.
+
       },
       headerTintColor: 'black',
       headerTitleStyle: {
-        fontWeight: 'bold',
+        // fontWeight: 'bold', //This however does change the title of the page and all sub pages.
+
+
       },
     },
 
