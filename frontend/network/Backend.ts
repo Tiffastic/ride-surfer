3c26bdd1d79b00cf54701039c04a4274ad7297fd
rachelbrough <rachel.brough@gmail.com>
2019-03-31 10:23:58 -0600

    real rebase fix

--- frontend/network/Backend.ts
+++ frontend/network/Backend.ts
@@ -1,6 +1,6 @@
-// const API_URL = "https://ride-surfer.herokuapp.com";
+const API_URL = "https://ride-surfer.herokuapp.com";
 
-const API_URL = "http://10.0.1.14:5000";
+// const API_URL = "http://10.0.1.14:5000";
 
 export function fetchAPI(path: string, info?: RequestInit) {
   console.log(`${(info && info.method) || "GET"} @ ${API_URL} to ${path}`);
