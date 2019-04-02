1f551e98eecfbe3607b5dffc27cf7195bc7a8db4
Ethan Ransom <derethanhausen@gmail.com>
2019-04-01 21:57:35 -0600

    Render list of Journeys, with map

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import mapboxgl from "mapbox-gl";
 
 import { fetchAPI } from "./Backend";
 import logo from "./logo.svg";
@@ -13,6 +14,142 @@ const pages: Page[] = [
   { name: "Users", render: () => <Users /> },
   { name: "Journeys", render: () => <Journeys /> }
 ];
+
+const saltLakeCenter = [40.7487805, -111.8718196].reverse() as [number, number];
+
+class JourneyMap extends React.Component<{
+  id: number;
+  origin: any;
+  destination: any;
+  path: any | null;
+}> {
+  componentDidMount() {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiZXRoYW5yYW4iLCJhIjoiY2pya3V6MGwyMDF1NzQzbXRnMHl3cGN5aiJ9.Y-iyykoUZuKEG7OyfRZDQw";
+    var map = new mapboxgl.Map({
+      container: this.domId(),
+      style: "mapbox://styles/mapbox/outdoors-v11",
+      center: saltLakeCenter,
+      zoom: 11
+    });
+
+    map.on("load", () => {
+      if (this.props.path !== null) {
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: this.props.path
+            }
+          },
+          layout: {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          paint: {
+            "line-color": "#888",
+            "line-width": 8
+          }
+        });
+      }
+
+      map.addLayer({
+        id: "points",
+        type: "symbol",
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: this.props.origin.coordinates.reverse()
+                },
+                properties: {
+                  title: "Start",
+                  icon: "monument"
+                }
+              },
+              {
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: this.props.destination.coordinates.reverse()
+                },
+                properties: {
+                  title: "End",
+                  icon: "harbor"
+                }
+              }
+            ]
+          }
+        },
+        layout: {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        }
+      });
+    });
+  }
+
+  private domId() {
+    return `map-${this.props.id}`;
+  }
+
+  render() {
+    return <section style={{ width: 500, height: 500 }} id={this.domId()} />;
+  }
+}
+
+class Journey extends React.Component<{ journey: any }, { expanded: boolean }> {
+  state = {
+    expanded: false
+  };
+
+  private toggleExpanded = () => {
+    this.setState(state => ({
+      expanded: !state.expanded
+    }));
+  };
+
+  render() {
+    return (
+      <li key={this.props.journey.id}>
+        <p>
+          Id: {this.props.journey.id} -{" "}
+          {this.props.journey.isDriver ? "Driver" : "Passenger"}
+        </p>
+        <p>
+          Created At: {new Date(this.props.journey.createdAt).toLocaleString()}
+        </p>
+        <p>
+          <button onClick={this.toggleExpanded}>
+            {this.state.expanded ? "Hide Map" : "Show Map"}
+          </button>
+        </p>
+        {this.state.expanded && (
+          <p>
+            <JourneyMap
+              id={this.props.journey.id as number}
+              origin={this.props.journey.origin}
+              destination={this.props.journey.destination}
+              path={this.props.journey.path}
+            />
+          </p>
+        )}
+      </li>
+    );
+  }
+}
 
 class Journeys extends React.Component {
   state = {
@@ -37,9 +174,7 @@ class Journeys extends React.Component {
 
         <ul>
           {this.state.journeys.map((journey: any) => (
-            <li key={journey.id}>
-              <p>Id: {journey.id}</p>
-            </li>
+            <Journey key={journey.id} journey={journey} />
           ))}
         </ul>
       </div>
